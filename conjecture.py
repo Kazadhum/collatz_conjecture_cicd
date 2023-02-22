368c33cb62f156f939b42974392ef9b42d5fe8a5
diogo <diogoscsv@gmail.com>
2023-02-22 11:41:24 +0000

    testing ci

--- conjecture.py
+++ conjecture.py
@@ -1,32 +1,32 @@
 def algorhythm():
 
     start_num = int(input("Select a starting number: \n"))
-    
+
     current_number = start_num
 
     step_number = 0
 
     while(current_number) != 1:
-        
+
         if (current_number % 2 == 0):
             is_even = True
-            print(f'{current_number} is even!')
+            print(f"{current_number} is even!")
         else:
             is_even = False
-            print(f'{current_number} is odd!')
-        
-        if (is_even == True):
-            print(f'{current_number} / 2 = {current_number/2}')
+            print(f"{current_number} is odd!")
+
+        if is_even:
+            print(f"{current_number} / 2 = {current_number/2}")
             current_number = current_number / 2
         else:
-            print(f'{current_number}*3 + 1 = {current_number*3 + 1}')
+            print(f"{current_number}*3 + 1 = {current_number*3 + 1}")
             current_number = current_number*3 + 1
 
         step_number = step_number + 1
 
-        print(f'Number of steps: {step_number}')
-        print('This project makes use of CI techniques')
+        print(f"Number of steps: {step_number}")
+        print("This project makes use of CI techniques")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     algorhythm()
